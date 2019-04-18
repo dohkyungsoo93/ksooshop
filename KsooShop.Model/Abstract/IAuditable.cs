4f4c51c40845b2ab94e392fc8c8641b73aee6b18
DohKyungsoo <trinhquyngoc.royal@gmail.com>
2019-04-18 15:27:07 +0700

    add model

---
+++ KsooShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsooShop.Model.Abstract
+{
+    interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { set; get; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { set; get; }
+        string MetaKeyword { set; get; }
+        string MetaDescripton { set; get; }
+        bool Status { set; get; }
+    }
+}
---
+++ KsooShop.Model/Models/Post.cs
@@ -0,0 +1,39 @@
+﻿using KsooShop.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsooShop.Model.Models
+{
+    [Table("Posts")]
+    public class Post :Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+        [Column(TypeName ="varchar")]
+        [Required]
+        [MaxLength(256)]
+        public string Alias { set; get; }
+        [Required]
+        public int CategoryID { set; get; }
+        [MaxLength(256)]
+        public string Image { set; get; }
+        [MaxLength(500)]
+        public string Descripton { set; get; }
+        public string Content { set; get; }
+        public bool? HomeFlag { set; get; }
+        public bool? HotFlag { set; get; }
+        public int? ViewCount { set; get; }
+        [ForeignKey("CategoryID")]
+        public virtual PostCategory PostCategory { set; get; }
+
+    }
+}
